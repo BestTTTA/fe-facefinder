32ba01c68e4ba3a28baf2152fa2080b256fb5067
BesteTTTA <bst12546@gmail.com>
2025-08-08 22:18:59 +0700

    maintance

--- src/app/event/[id]/page.js
+++ src/app/event/[id]/page.js
@@ -41,5 +41,19 @@ export async function generateMetadata({ params }) {
 }
 export default async function Page({ params }) {
   const { id } = await params;
-  return <EventPageClient eventId={id} />;
+  // return <EventPageClient eventId={id} />;
+  return (
+ <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+  <div className="bg-white rounded-2xl shadow-lg p-6 max-w-sm text-center">
+    <h2 className="text-xl font-bold mb-3">🚧 กำลังปรับปรุงระบบ<br />🚧 System Maintenance</h2>
+    <p className="text-gray-600 mb-5">
+      ขณะนี้ระบบกำลังอยู่ในระหว่างการปรับปรุง<br />
+      The system is currently under maintenance.<br />
+      กรุณากลับมาใหม่อีกครั้งในภายหลัง<br />
+      Please try again later.
+    </p>
+  </div>
+</div>
+
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,24 @@
-"use client"
+"use client";
 import Carousel from "@/components/Carousel";
 import Search from "@/components/Search";
 import ShowEvent from "@/components/ShowEvent";
 import AlertBar from "@/components/AlertBar";
 
 export default function Home() {
-
   return (
     <div className="flex flex-col gap-9 bg-surface h-screen relative">
+ <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+  <div className="bg-white rounded-2xl shadow-lg p-6 max-w-sm text-center">
+    <h2 className="text-xl font-bold mb-3">🚧 กำลังปรับปรุงระบบ<br />🚧 System Maintenance</h2>
+    <p className="text-gray-600 mb-5">
+      ขณะนี้ระบบกำลังอยู่ในระหว่างการปรับปรุง<br />
+      The system is currently under maintenance.<br />
+      กรุณากลับมาใหม่อีกครั้งในภายหลัง<br />
+      Please try again later.
+    </p>
+  </div>
+</div>
 
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-2xl shadow-lg p-6 max-w-sm text-center">
-            <h2 className="text-xl font-bold mb-3">🚧 กำลังปรับปรุงระบบ</h2>
-            <p className="text-gray-600 mb-5">
-              ขณะนี้ระบบกำลังอยู่ในระหว่างการปรับปรุง กรุณากลับมาใหม่อีกครั้งในภายหลัง
-            </p>
-
-          </div>
-        </div>
       <main className="flex flex-col items-center max-w-full gap-8">
         {/* <AlertBar /> */}
         {/* <Carousel /> */}
